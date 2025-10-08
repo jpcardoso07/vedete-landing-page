bd97d49ab123b4b9d1d372f1aac6567c8cc4fa04
jpcardoso07 <0zerodivisionerror0@gmail.com>
2025-10-07 22:16:09 -0300

    feat: initial navbar

--- src/modules/home/presentation.tsx
+++ src/modules/home/presentation.tsx
@@ -2,7 +2,7 @@ import type { IHomePresentation } from "./interface";
 
 export default function HomePresentation(state: Readonly<IHomePresentation>) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full py-16 px-8 md:px-12 xl:px-16 bg-[#0f0f0f] text-white">
       <p>teste</p>
     </div>
   );
--- src/shared/components/navbar.tsx
+++ src/shared/components/navbar.tsx
@@ -1,3 +1,21 @@
 export default function Navbar() {
-  return <div></div>;
+  return (
+    <div className="flex items-center justify-between py-6 px-8 md:px-12 xl:px-16 h-[80px] bg-[#0f0f0f] text-white">
+      <span className="text-5xl font-bold italic">Vedete</span>
+      <div className="flex items-center gap-6">
+        <a target="_blank" href="" className="text-lg hover:font-semibold">
+          Produtos
+        </a>
+        <a target="_blank" href="" className="text-lg hover:font-semibold">
+          Sobre nós
+        </a>
+        <a target="_blank" href="" className="text-lg hover:font-semibold">
+          FAQ
+        </a>
+        <a target="_blank" href="" className="text-lg hover:font-semibold">
+          Contato
+        </a>
+      </div>
+    </div>
+  );
 }
