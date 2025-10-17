97efbdf56c650d97f754b9fe3a1a43a38ce81d77
jpcardoso07 <0zerodivisionerror0@gmail.com>
2025-10-16 22:12:36 -0300

    feat: implement animation and style in home page

--- src/modules/home/presentation.tsx
+++ src/modules/home/presentation.tsx
@@ -1,24 +1,36 @@
 import type { IHomePresentation } from "./interface";
-import Refri1 from "../../shared/assets/refri1.png";
-import Refri2 from "../../shared/assets/refri2.png";
+import Refri1 from "../../shared/assets/lata-guarana.png";
+import Refri2 from "../../shared/assets/lata-vedete.png";
 
 export default function HomePresentation(state: Readonly<IHomePresentation>) {
   return (
     <div className="flex flex-col items-center justify-center w-full py-16 px-8 md:px-12 xl:px-16 bg-[#0f0f0f] text-white">
       <div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-12 w-full max-w-[1300px]">
-        <div id="carousel" className="flex max-w-[500px] w-full">
-          <img src={Refri1} alt="Refri 1" className="w-[32%] xl:w-[40%]" />
-          <img src={Refri2} alt="Refri 2" className="w-[32%] xl:w-[40%]" />
-          <img src={Refri1} alt="Refri 3" className="w-[32%] xl:w-[40%]" />
+        <div
+          id="carousel"
+          className="flex max-w-[500px] w-full mt-[-12%] lg:mt-[-2%]"
+        >
+          <img
+            src={Refri1}
+            alt="Refri 1"
+            className="w-[50%] h-fit pt-[10%] transition-transform duration-500 ease-out hover:scale-110"
+          />
+          <img
+            src={Refri2}
+            alt="Refri 2"
+            className="w-[60%] transition-transform duration-500 ease-out hover:scale-110"
+          />
         </div>
         <div className="flex flex-col gap-8">
-          <p className="text-5xl lg:text-6xl font-bold text-[#2933c2]">
+          <p className="text-5xl lg:text-6xl font-bold text-blue-700">
             O Sabor do <br /> Brasileiro
           </p>
-          <p className="text-lg lg:text-xl max-w-[460px] lg:max-w-[500px]">
-            Tradição no almoço de familia, churrasco e aniversários. Quem é
+          <p className="text-base lg:text-lg max-w-[460px] lg:max-w-[480px]">
+            Tradição no almoço de família, churrasco e aniversários. Quem é
             brasileiro sabe que Vedete acompanha diferentes fases da nossa
-            trajetória.
+            trajetória. <br /> <br /> Refrescante, marcante e feita para estar
+            presente em todos os momentos que têm alegria, amizade e boas
+            risadas.
           </p>
         </div>
       </div>
