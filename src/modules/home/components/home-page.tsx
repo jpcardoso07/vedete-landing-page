114509113b09fa6425ac0f191d22c4b370152899
jpcardoso07 <0zerodivisionerror0@gmail.com>
2025-10-19 18:02:47 -0300

    fix: adjust home page responsiviness

---
+++ src/modules/home/components/home-page.tsx
@@ -0,0 +1,38 @@
+import Refri1 from "../../../shared/assets/lata-guarana.png";
+import Refri2 from "../../../shared/assets/lata-vedete.png";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full py-16 px-8 md:px-12 xl:px-16 bg-[#0f0f0f] text-white mt-[-80px]">
+      <div className="flex flex-col-reverse lg:flex-row items-center justify-between gap-12 w-full max-w-[1300px]">
+        <div
+          id="carousel"
+          className="flex max-w-[500px] w-full mt-[-12%] lg:mt-[-2%]"
+        >
+          <img
+            src={Refri1}
+            alt="Refri 1"
+            className="w-[50%] xl:w-[60%] h-fit pt-[10%] xl:pt-[12%] transition-transform duration-500 ease-out hover:scale-110"
+          />
+          <img
+            src={Refri2}
+            alt="Refri 2"
+            className="w-[60%] xl:w-[72%] transition-transform duration-500 ease-out hover:scale-110"
+          />
+        </div>
+        <div className="flex flex-col gap-8">
+          <p className="text-5xl lg:text-6xl font-bold text-blue-700">
+            O Sabor do <br /> Brasileiro
+          </p>
+          <p className="text-base lg:text-lg max-w-[460px] lg:max-w-[480px]">
+            Tradição no almoço de família, churrasco e aniversários. Quem é
+            brasileiro sabe que Vedete acompanha diferentes fases da nossa
+            trajetória. <br /> <br /> Refrescante, marcante e feita para estar
+            presente em todos os momentos que têm alegria, amizade e boas
+            risadas.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
