95fb9618309179aa759d2cbd2c18939d38838bd9
jpcardoso07 <0zerodivisionerror0@gmail.com>
2025-10-19 19:23:14 -0300

    adjust directories

--- src/modules/home/presentation.tsx
+++ src/modules/home/presentation.tsx
@@ -1,10 +1,11 @@
-import HomePage from "./components/home-page";
-import type { IHomePresentation } from "./interface";
+import HomePage from "./pages/home-page";
+import ProductsPage from "./pages/products-page";
 
-export default function HomePresentation(state: Readonly<IHomePresentation>) {
+export default function HomePresentation() {
   return (
     <div className="flex flex-col w-full bg-surface relative">
       <HomePage />
+      <ProductsPage />
     </div>
   );
 }
