c8930aba90095b2dc7cc14e86dcbd553715ff1e6
Pedro <ph.oliveira.depaula@gmail.com>
2025-10-21 15:29:17 -0300

    fix: about us page responsiviness

--- src/modules/home/pages/about-us-page.tsx
+++ src/modules/home/pages/about-us-page.tsx
@@ -8,13 +8,13 @@ export default function AboutUsPage() {
         <p className="font-bold text-4xl text-blue-700">Sobre Nós</p>
         <p className="text-xl">Descubra um pouco mais sobre nossa história</p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-20 w-full max-w-[1300px] mt-16">
-        <div className="pb-12">
-          <p className="text-xl mb-4">
+      <div className="flex flex-col-reverse lg:grid lg:grid-cols-2 items-center lg:gap-20 w-full max-w-[1300px] mt-16">
+        <div className="lg:pb-12 max-w-xl">
+          <p className="text-lg lg:text-xl mb-4 mt-10 lg:mt-0">
             De almoços em família a festas de aniversário, a Vedete está
             presente em cada brinde, em cada risada e em cada lembrança boa.
           </p>
-          <ul className="space-y-3 text-lg">
+          <ul className="space-y-3 text-base lg:text-lg">
             <li>
               <strong>Missão:</strong> levar alegria e sabor à vida das pessoas,
               valorizando momentos simples e especiais.
@@ -29,12 +29,18 @@ export default function AboutUsPage() {
             </li>
           </ul>
         </div>
-        <img src={FabricaVedete} className="rounded-xl hover:scale-104 transition-all duration-300" />
+        <img
+          src={FabricaVedete}
+          className="rounded-xl hover:scale-104 transition-all duration-300 w-full max-w-xl"
+        />
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-20 w-full max-w-[1300px] mt-20">
-        <img src={DonoVedete} className="rounded-xl hover:scale-104 transition-all duration-300" />
-        <div className="pb-12">
-          <p className="text-xl mb-4">
+      <div className="flex flex-col lg:grid lg:grid-cols-2 items-center lg:gap-20 w-full max-w-[1300px] mt-20">
+        <img
+          src={DonoVedete}
+          className="rounded-xl hover:scale-104 transition-all duration-300 w-full max-w-xl"
+        />
+        <div className="lg:pb-12">
+          <p className="text-lg lg:text-xl mb-4 mt-10 lg:mt-0 max-w-xl">
             Desde 1984, a Vedete faz parte da mesa dos brasileiros. Nascida em
             uma pequena fábrica do interior de São Paulo, a marca cresceu
             mantendo o mesmo compromisso: oferecer sabor e qualidade com aquele
